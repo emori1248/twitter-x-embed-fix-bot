b08c50b4a5a9976cbf0e932af8ade17a1d6820e8
Ethan Mori <egmori2@gmail.com>
2025-01-05 20:10:13 -0500

    Fix bug with content before link not triggering filter

--- main.py
+++ main.py
@@ -10,28 +10,37 @@ class MyClient(discord.Client):
 
     async def on_message(self, message):
         if not message.author.bot:
-            u = urlparse(message.content)
+            # Split message content into words and look for URLs
+            words = message.content.split()
+            found_url = False
+            new_content = []
+            
+            for word in words:
+                u = urlparse(word)
+                if ('twitter.com' in u.netloc or 'x.com' in u.netloc or 
+                    'instagram.com' in u.netloc or 'tiktok.com' in u.netloc):
+                    found_url = True
+                    
+                    if u.netloc == 'twitter.com':
+                        u = u._replace(netloc='fxtwitter.com')
+                    elif u.netloc == 'x.com':
+                        u = u._replace(netloc='fixupx.com')
+                    elif u.netloc == 'instagram.com' or u.netloc == 'www.instagram.com':
+                        u = u._replace(netloc='ddinstagram.com')
+                    elif u.netloc == 'tiktok.com' or u.netloc == 'www.tiktok.com':
+                        u = u._replace(netloc='tnktok.com')
+                        
+                    new_content.append(urlunparse(u))
+                else:
+                    new_content.append(word)
+                    
+            if found_url:
+                # Delete the old user message in case a partially functioning embed is present
+                await message.delete()
 
-            if not ('twitter.com' in u.netloc or 'x.com' in u.netloc or 'instagram.com' in u.netloc or 'tiktok.com' in u.netloc):
-                return
-
-            if u.netloc == 'twitter.com':
-                u = u._replace(netloc='fxtwitter.com')
-            elif u.netloc == 'x.com':
-                u = u._replace(netloc='fixupx.com')
-
-            elif u.netloc == 'instagram.com' or u.netloc == 'www.instagram.com':
-                u = u._replace(netloc='ddinstagram.com')
-
-            elif u.netloc == 'tiktok.com' or u.netloc == 'www.tiktok.com':
-                u = u._replace(netloc='tnktok.com')
-                
-            # Delete the old user message in case a partially functioning embed is present
-            await message.delete()
-
-            # Append the redirected link to a formatted message to indicate the original author after their message is deleted
-            content = message.author.mention + ": " + urlunparse(u)
-            await message.channel.send(content, silent=True)
+                # Append the redirected link to a formatted message to indicate the original author after their message is deleted
+                content = message.author.mention + ": " + " ".join(new_content)
+                await message.channel.send(content, silent=True)
             
             
 
